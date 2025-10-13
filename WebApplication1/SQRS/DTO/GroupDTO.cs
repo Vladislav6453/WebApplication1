2f27150da0b28844e3b95fcddbe67a5004555791
Admin <Admin@CAB301-4-2024>
2025-10-13 18:00:41 +1000

    Добавьте файлы проекта.

---
+++ WebApplication1/SQRS/DTO/GroupDTO.cs
@@ -0,0 +1,15 @@
+﻿using WebApplication1.DB;
+
+namespace WebApplication1.SQRS.DTO
+{
+    public class GroupDTO
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public int? IdSpecial { get; set; }
+
+        public virtual Special? IdSpecialNavigation { get; set; }
+    }
+}
---
+++ WebApplication1/SQRS/GetListCommand.cs
@@ -0,0 +1,28 @@
+﻿using MyMediator.Interfaces;
+using WebApplication1.DB;
+using WebApplication1.SQRS.DTO;
+
+namespace WebApplication1.SQRS
+{
+    public class GetListStudentByGroupCommand : IRequest<IEnumerable<StudentDTO>>
+    {
+        public int IdStudent { get; set }
+
+        public class GetListStudentByGroupCommandHandler :
+            IRequestHandler<GetListStudentByGroupCommand, IEnumerable<StudentDTO>>
+        {
+
+            private readonly Db131025Context db;
+            public GetListStudentByGroupCommandHandler(Db131025Context db)
+            {
+                this.db = db;
+            }
+            public async Task<IEnumerable<StudentDTO>> HandleAsync(GetListStudentByGroupCommand request,
+                CancellationToken ct = default)
+            {
+                return await db.Students.Where(s => s.IdGroup == request.);
+
+            }
+        }
+    }
+}
